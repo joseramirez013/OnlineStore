068e0f1c305afbcd39325a797d96f8d49c17183b
joseramirez013 <joseramirez013@hotmail.com>
2021-02-14 08:23:23 -0600

    Upload and dowload files

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,13 @@
+export * from './advertising.controller';
 export * from './brand-product.controller';
 export * from './brand.controller';
 export * from './category-product.controller';
 export * from './category.controller';
+export * from './customer-shopping-cart.controller';
+export * from './customer-user.controller';
 export * from './customer.controller';
+export * from './file-download.controller';
+export * from './file-upload.controller';
 export * from './image-product.controller';
 export * from './ping.controller';
 export * from './product-brand.controller';
@@ -12,11 +17,9 @@ export * from './product-sale-item.controller';
 export * from './product.controller';
 export * from './sale-item-product.controller';
 export * from './sale-item-shopping-cart.controller';
+export * from './shopping-cart-customer.controller';
 export * from './shopping-cart-sale-document.controller';
 export * from './shopping-cart-sale-item.controller';
-export * from './customer-shopping-cart.controller';
-export * from './shopping-cart-customer.controller';
-export * from './customer-user.controller';
 export * from './user-customer.controller';
 export * from './user.controller';
-export * from './advertising.controller';
+
